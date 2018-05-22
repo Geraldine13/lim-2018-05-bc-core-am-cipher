43575ae24fcb46f3486553c3ecef48f5dbb731d8
Geraldine13 <geral1312@hotmail.com>
2018-05-22 13:04:41 -0500

    Subiendo el primer avance

---
+++ src/index.js
@@ -0,0 +1,8 @@
+alert("Bienvenido a la app de Cifrado");
+
+// Ingreso de usuario
+var name = prompt("Ingresa tu nombre");
+var userName = document.getElementById("user");
+userName.textContent = "Bienvenid@ " + name;
+
+//Elija una opción: Cifrar o descifrar
