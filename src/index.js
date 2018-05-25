1c78d938e1c3f4a9077b1d8fc4fbd6e087d83eb4
Geraldine13 <geral1312@hotmail.com>
2018-05-25 10:21:22 -0500

    Index funcionando el cifrado

--- src/index.js
+++ src/index.js
@@ -7,60 +7,46 @@
 
 
 // Declarando variable para el ingreso del texto
-var phrase = prompt('Ingrese texto');
-//document.getElementById('inputC').value;
-//
-//
-//
+var phrase = document.getElementById('inputC').value;
+
 phrase = phrase.toUpperCase();
 console.log(phrase);
 
 // Declarando variable para el offset nro de espacios
-var spaces = parseInt(prompt('Indique el offset'));
-    //document.getElementById('offset').value);
-//
-//
-
-
-
+var spaces = parseInt(document.getElementById('offset').value);
 
 // Función para el cifrado
 function cypher (phrase, spaces) {
 
- var answer = "";
- var initialASCII = 0;  // Código ASCII del texto ingresado
- var lastASCII = 0;     // Nuevo código ASCII 
-
-// Ciclo para todo el texto ingresado
- for (var n = 0; n < phrase.length; n++) {
-
-    if (phrase[n].charCodeAt() != 32) {
-
-       initialASCII = phrase[n].charCodeAt();
-       console.log(initialASCII);
-
-       lastASCII = (initialASCII - 65 + spaces) % 26 + 65;
-       console.log(lastASCII);
-
-       var a = String.fromCharCode(lastASCII);
-       console.log(a);
-
-       answer = answer.concat(a);
-
-    } else {
-
-       answer = answer + " ";
+    var answer = "";
+    var initialASCII = 0;  // Código ASCII del texto ingresado
+    var lastASCII = 0;     // Nuevo código ASCII 
+   
+   // Ciclo para todo el texto ingresado
+    for (var n = 0; n < phrase.length; n++) {
+   
+       if (phrase[n].charCodeAt() != 32) {
+   
+          initialASCII = phrase[n].charCodeAt();
+          lastASCII = (initialASCII - 65 + spaces) % 26 + 65;
+          var a = String.fromCharCode(lastASCII);
+          answer = answer.concat(a);
+   
+       } else {
+   
+          answer = answer + " ";
+       }
     }
-  }
-  return answer;
+     return answer;
 }
-document.getElementById('resultado').value = (cypher(phrase, spaces));
+   
+var boton = document.getElementById('botonC');
 
-//var boton = document.getElementById('botonC');
-//boton.addEventListener("click", result);
+function result() {
+    var phrase = document.getElementById('inputC').value;
+    var spaces = parseInt(document.getElementById('offset').value);
+    //boton = alert('Boton funciona');
+    document.getElementById('resultado').value = (cypher(phrase, spaces));
+}
 
-//function result() {
-    
-//}
-
-
+boton.addEventListener("click", result);
