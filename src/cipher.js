ac558fd196b04b76ebd20adb174b2e55e9e40464
Geraldine13 <geral1312@hotmail.com>
2018-05-30 11:06:09 -0500

    Modificaciones a los archivos html - index.js - cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -3,18 +3,18 @@ window.cipher = {
 // Función para el cifrado
 encode : (offset, string) => {
 
-          var answer = "";       // Variable para la respuesta
-          var initialASCII = 0;  // Código ASCII del texto ingresado
-          var lastASCII = 0;     // Nuevo código ASCII 
- 
+          let answer = "";       // Variable para la respuesta
+          let initialASCII = 0;  // Código ASCII del texto ingresado
+          let lastASCII = 0;     // Nuevo código ASCII 
+          string = string.toUpperCase();
           // Ciclo para todo el texto ingresado
-          for (var n = 0; n < string.length; n++) {
+          for (let n = 0; n < string.length; n++) {
  
             if (string[n].charCodeAt() != 32) {
  
               initialASCII = string[n].charCodeAt();
               lastASCII = (initialASCII - 65 + offset) % 26 + 65;
-              var a = String.fromCharCode(lastASCII);
+              let a = String.fromCharCode(lastASCII);
               answer = answer.concat(a);
  
             } else {
@@ -29,18 +29,18 @@ encode : (offset, string) => {
 // Función para descifrar
 decode : (offset, string) => {
 
-          var answer = "";
-          var antASCII = 0;       // Código ASCII del texto ingresado a convertir
-          var initialASCII = 0;   // Nuevo código ASCII 
- 
+          let answer = "";
+          let antASCII = 0;       // Código ASCII del texto ingresado a convertir
+          let initialASCII = 0;   // Nuevo código ASCII 
+          string = string.toUpperCase();
           // Ciclo para todo el texto ingresado
-          for (var n = 0; n < string.length; n++) {
+          for (let n = 0; n < string.length; n++) {
  
             if (string[n].charCodeAt() != 32) {
  
               antASCII = string[n].charCodeAt();
               initialASCII = (antASCII - 90 - offset) % 26 + 90;
-              var a = String.fromCharCode(initialASCII);
+              let a = String.fromCharCode(initialASCII);
               answer = answer.concat(a);
  
             } else {
@@ -53,15 +53,15 @@ decode : (offset, string) => {
 
 // Función para el botón del resultado del encode
 resultCipher : () => {
-          var string = document.getElementById('input').value;
-          var offset = parseInt(document.getElementById('offset').value);
+          let string = document.getElementById('input').value;
+          let offset = parseInt(document.getElementById('offset').value);
           showResults.innerHTML = (cipher.encode(offset, string));
   },
 
 // Función para el botón del resultado del decode
 resultDecipher : () => {
-    var string = document.getElementById('input').value;
-    var offset = parseInt(document.getElementById('offset').value);
+    let string = document.getElementById('input').value;
+    let offset = parseInt(document.getElementById('offset').value);
     showResults.innerHTML = (cipher.decode(offset, string));
   },
         
--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
-//alert("Bienvenido a la app de Cifrado");
-
-/* Ingreso de usuario
-var name = prompt("Ingresa tu nombre");
-var userName = document.getElementById("user");
-userName.textContent = "Bienvenid@ " + name.toUpperCase();
-*/
-
 // Declarando variable para el ingreso del texto
-var string = document.getElementById('input').value;
-
-//Convirtiendo a mayúsculas el texto ingresado
-string = string.toUpperCase();
-console.log(string);
+let string = document.getElementById('input');
 
 // Declarando variable para el offset nro de espacios
-var offset = parseInt(document.getElementById('offset').value);
+let offset = parseInt(document.getElementById('offset'));
 
 // Variables para ejecutar el botón Cifrar y Decifrar
-var botonCipher = document.getElementById('botonC');
-var botonDecipher = document.getElementById('botonD');
+const botonCipher = document.getElementById('botonC');
+const botonDecipher = document.getElementById('botonD');
 
 // Declarando la variable para mostrar los resultados
-var showResults = document.getElementById('resultado');
+const showResults = document.getElementById('resultado');
 
 // Escuchando el evento del Click para el cifrado y decifrado
 botonCipher.addEventListener("click", cipher.resultCipher);
